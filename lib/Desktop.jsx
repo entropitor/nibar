d27ce3f4ebf85eb17ece4b1e4079c86838e01e01
Jens Claes <jens@datacamp.com>
2020-01-20 09:26:59 +0100

    Fix: show labels

--- lib/Desktop.jsx
+++ lib/Desktop.jsx
@@ -8,24 +8,49 @@ const containerStyle = {
 };
 
 const desktopStyle = {
-  width: "4ch",
+  textAlign: "center",
+  paddingLeft: "1ch",
+  color: '#ccc'
 };
 
-const renderSpace = (index, focused, visible, windows) => {
+const LABELS = {
+  1: "🦊",
+  2: "📜",
+  3: "💻",
+  4: "➕",
+  5: "➕",
+  6: "➕",
+  7: "➕",
+  8: "✉️",
+  9: "#️⃣"
+}
+const LABEL_REGEX = /(\d)+:@/
+const annotateLabel = label => {
+  const groups = label.match(LABEL_REGEX)
+  if (!groups) {
+    return label
+  }
+  const number = groups[1]
+  return `${number}:${LABELS[number]}`
+}
+const fixLabel = label => annotateLabel(label.split(':').reverse().join(':'))
+const renderSpace = ({space}) => {
+  const {index, focused, visible, windows, label} = space;
   let contentStyle = JSON.parse(JSON.stringify(desktopStyle));
   let hasWindows = windows.length > 0;
   if (focused == 1) {
     contentStyle.color = styles.colors.accent;
     contentStyle.fontWeight = "700";
   } else if (visible == 1) {
-    contentStyle.color = styles.colors.fg;
+    contentStyle.color = styles.colors.red;
+  } else if (!hasWindows) {
+    contentStyle.color = styles.colors.empty;
   }
   const onClick = () => run(`/usr/local/bin/yabai -m space --focus ${index}`)
 
   return (
     <div style={contentStyle} onClick={onClick}>
-      &nbsp;
-      {hasWindows ? `[${index}]` : `-${index}-`}
+      {label != "" ? fixLabel(label) : hasWindows ? `[ ${index} ]` : `-${index}:?-`}
     </div>
   );
 };
@@ -36,7 +61,7 @@ const render = ({ output }) => {
   const spaces = [];
 
   output.forEach(function(space) {
-    spaces.push(renderSpace(space.index, space.focused, space.visible, space.windows));
+    spaces.push(renderSpace({space}));
   });
 
   return (
--- lib/styles.jsx
+++ lib/styles.jsx
@@ -4,7 +4,8 @@ export default {
     dim: "#6c6c6c",
     bg: "#2d2d2d",
     red: "#ff8700",
-    accent: "#5fafaf"
+    accent: "#5fafaf",
+    empty: "#3d3d3d",
   },
   fontSize: "13px",
   lineHeight: "20px",
